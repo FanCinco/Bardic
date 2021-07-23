4e9f59544837d581dc20ecf70d631e1ac949b37c
Francisco Puma <francis_pancho12@hotmail.com>
2021-07-22 20:19:32 -0700

    working on the places routes

--- controllers/places-routes.js
+++ controllers/places-routes.js
@@ -2,3 +2,85 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Places, Story, Expenses, Users, Vote } = require('../models');
 
+// get all places 
+router.get('/', (req, res) => {
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'Description', 
+      'Name',
+      'created_at',
+      //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPlacesData => {
+      const posts = dbPlacesData.map(post => post.get({ plain: true }));
+
+      res.render('places', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single post
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'Description', 
+      'Name',
+      'created_at',
+      //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPlacesData => {
+      if (!dbPlacesData) {
+        res.status(404).json({ message: 'No matching place found with this id' });
+        return;
+      }
+
+      const post = dbPlacesData.get({ plain: true });
+
+      res.render('single-place', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// router.get('/login', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/');
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+module.exports = router;
+
