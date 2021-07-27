4ea90ee9027f0b73fe83d61f5719e2951cc52e33
Francisco Puma <francis_pancho12@hotmail.com>
2021-07-26 21:30:11 -0700

    adding user trip routes

--- controllers/api/User-Trip-routes.js
+++ controllers/api/User-Trip-routes.js
@@ -4,111 +4,112 @@ const { Places, Stories, Expenses, Users, Vote } = require('../models');
 //insert cons for password package
 
 
-// // get all 
-// router.get('/', (req, res) => {
-//     console.log('======================');
-//     Comments.findAll({
-//         attributes: [
-//             'id',
-//             'content',
-//             'created_at',
-//             //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE stories.id = vote.stories_id)'), 'vote_count']
-//         ],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//         .then(dbCommentsData => res.json(dbCommentsData))
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+// get all 
+router.get('/', (req, res) => {
+    console.log('======================');
+    UserTrip.findAll({
+        attributes: [
+            'id',
+            'user_id',
+            'trip_id',
+            'created_at',
+            //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE usertrip.id = vote.usertrip_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbUserTripData => res.json(dbUserTripData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 
-// //get one
+//get one
 
-// router.get('/:id', (req, res) => {
-//     Comments.findOne({
-//         where: {
-//             id: req.params.id
-//         },
-//         attributes: [
-//             'id',
-//             'content',
-//             'created_at',
-//             //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE stories.id = vote.stories_id)'), 'vote_count']
-//         ],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//         .then(dbCommentsData => {
-//             if (!dbCommentsData) {
-//                 res.status(404).json({ message: 'No matching data found with this id' });
-//                 return;
-//             }
-//             res.json(dbCommentsData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
-
-
-
+router.get('/:id', (req, res) => {
+    UserTrip.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'user_id',
+            'trip_id',
+            'created_at',
+            //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE usertrip.id = vote.usertrip_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbUserTripData => {
+            if (!dbUserTripData) {
+                res.status(404).json({ message: 'No matching data found with this id' });
+                return;
+            }
+            res.json(dbUserTripData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
-// // create new entries
 
 
-// router.post('/', (req, res) => {
-//     Comments.create({
-//         content: req.body.content,
-//         user_id: req.session.user_id,
-//         post_id: req.body.post_id
-//     })
-//         .then(dbCommentsData => res.json(dbCommentsData))
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+
+// create new entries
 
 
-// // Update 
+router.post('/', (req, res) => {
+    UserTrip.create({
+        user_id: req.session.user_id,
+        trip_id: req.body.trip_id
+    })
+        .then(dbUserTripData => res.json(dbUserTripData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-// router.put('/:id', (req, res) => {
-//     Comments.update(
-//         {
-//             title: req.body.title
-//         },
-//         {
-//             where: {
-//                 id: req.params.id
-//             }
-//         }
-//     )
-//         .then(dbCommentsData => {
-//             if (!dbCommentsData) {
-//                 res.status(404).json({ message: 'No matching data found with this id' });
-//                 return;
-//             }
-//             res.json(dbCommentsData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+
+// Update 
+
+router.put('/:id', (req, res) => {
+    UserTrip.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbUserTripData => {
+            if (!dbUserTripData) {
+                res.status(404).json({ message: 'No matching data found with this id' });
+                return;
+            }
+            res.json(dbUserTripData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 
@@ -117,17 +118,17 @@ const { Places, Stories, Expenses, Users, Vote } = require('../models');
 
 // router.delete('/:id', (req, res) => {
 //     console.log('id', req.params.id);
-//     Comments.destroy({
+//     UserTrip.destroy({
 //         where: {
 //             id: req.params.id
 //         }
 //     })
-//         .then(dbCommentsData => {
-//             if (!dbCommentsData) {
+//         .then(dbUserTripData => {
+//             if (!dbUserTripData) {
 //                 res.status(404).json({ message: 'No matching data found with this id' });
 //                 return;
 //             }
-//             res.json(dbCommentsData);
+//             res.json(dbUserTripData);
 //         })
 //         .catch(err => {
 //             console.log(err);
