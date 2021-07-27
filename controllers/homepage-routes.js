3843a5c180ee52153d507b7c1dcbc9aadfe6fb4e
Francisco Puma <francis_pancho12@hotmail.com>
2021-07-27 16:13:49 -0700

    making final edits

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -14,4 +14,15 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
 
+
+// Homepage
+router.get('/homepage', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
   module.exports = router;
