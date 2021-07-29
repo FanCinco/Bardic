94d4d06cf5015e369ce5602bdbdf70cc4571c33a
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-07-28 19:10:58 -0700

    fixing code

--- public/javascript/add-trips.js
+++ public/javascript/add-trips.js
@@ -1,25 +1,25 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const trip_id = document.querySelector('input[name="trips-trip_id"]').value;
-    const user_id = document.querySelector('input[name="trips-user_id"]').value;
-  
-    const response = await fetch(`/api/trips`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        place_id,
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/homepage');
-    } else {
-      alert(response.statusText);
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="trips-title"]').value;
+  const place_id = document.querySelector('input[name="trips-place_id"]').value;
+
+  const response = await fetch(`/api/trips`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      place_id
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/homepage');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.new-trip').addEventListener('submit', newFormHandler);
+}
+
+document.querySelector('.new-trip').addEventListener('submit', newFormHandler);
--- public/javascript/edit-trips.js
+++ public/javascript/edit-trips.js
@@ -1,27 +1,28 @@
+  
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const trip_id = document.querySelector('input[name="trips-trip_id"]').value.trim();
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+  event.preventDefault();
 
-    const response = await fetch(`/api/trips/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        title,
-        place_id,
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/homepage/');
-    } else {
-      alert(response.statusText);
+  const title = document.querySelector('input[name="trips-title"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/trips/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      place_id,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.replace('/homepage/');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.edit-trips').addEventListener('submit', editFormHandler);
+}
+
+document.querySelector('.edit-trips').addEventListener('submit', editFormHandler);
