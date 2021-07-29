d098b760b5f223a91083553da820d358de7cdeb7
Kiefer Levine <kiefer_8@hotmail.com>
2021-07-29 19:25:54 -0400

    Reverted user seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,27 +2,27 @@ const { User } = require('../models');
 
 const userData = [
     {
-        firstName: 'Justin',
-        lastName: 'Weicht',
-        email: 'justinweicht11@gmail.com',
+        firstName: 'George',
+        lastName: 'Michael',
+        email: 'gomich@gmail.com',
         password: '$2b$12$0dHi6mBSkMU3mkWEjjPiz.YzOwUc9RHCWvhq0dJE8eALahdUO0ubW'
     },
     {
-        firstName: 'Kiefer',
-        lastName: 'Levine',
-        email: 'kiefer_8@hotmail.com',
+        firstName: 'Havier',
+        lastName: 'Martinez',
+        email: 'hart@yahoo.ca',
         password: '$2b$12$0jgWhrq52d.nhtYlARflH.E/ueMDEbIo0Sj5tlu1Eipv8pVdCCVj.'
     },
     {
-        firstName: 'Said',
-        lastName: 'Hadad',
-        email: 'saiddavid.hadad@gmail.com',
+        firstName: 'Miranda',
+        lastName: 'Shumacher',
+        email: 'schum@hotmail.com',
         password: '$2b$12$TSiJ.VI/48UcfHMnJO840.ea9YrjkmzdbZewScbka3VWi9HpwrUEW'
     },
     {
-        firstName: 'Francisco',
-        lastName: 'Puma',
-        email: 'francis_pancho12@hotmail.com',
+        firstName: 'Tina',
+        lastName: 'Bernette',
+        email: 'pyrrhic@outlook.com',
         password: '$2b$12$XqWO8yhw9hOJfoduT9KUseNVzYBEjqz29XLLYWeyEbgDV3QQ6FhY6'
     }
 ];
