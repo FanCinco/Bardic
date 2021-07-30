300a8f3df81d80e749c713a6419f1454e21769d9
Kiefer Levine <kiefer_8@hotmail.com>
2021-07-29 21:35:40 -0400

    Disabled password validation

--- controllers/api/users-routes.js
+++ controllers/api/users-routes.js
@@ -142,10 +142,10 @@ router.post('/login', (req, res) => {
 
     const validPassword = dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
+    // if (!validPassword) {
+    //   res.status(400).json({ message: 'Incorrect password!' });
+    //   return;
+    // }
 
     req.session.save(() => {
       req.session.user_id = dbUserData.id;
